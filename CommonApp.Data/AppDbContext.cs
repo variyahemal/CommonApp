c79d81b0c12d36393beaafd8654fbdb5ea4ca646
Hemal Variya <variy-he@in001.itgr.net>
2023-03-30 10:42:02 +0530

    Add project files.

---
+++ CommonApp.Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonApp.Data
+{
+    public class AppDbContext : DbContext, IDisposable
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=aspnet-CommonApp;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /* Seed */
+
+            /* Configurations */
+        }
+
+        /* Model Dbset */
+
+    }
+}
---
+++ CommonApp.Data/Migrations/20230330051040_Initial-setup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommonApp.Data.Migrations
+{
+    public partial class Initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
